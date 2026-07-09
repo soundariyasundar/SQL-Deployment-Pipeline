c8cff925b0865dfa9b21f9a44f0128ec749dd240
soundariya sundar <154726977+soundariyasundar@users.noreply.github.com>
2026-07-09 20:06:57 +0530

    Replace DELETE with INSERT for employee records

--- cicd-sql/queries/update_salary.sql
+++ cicd-sql/queries/update_salary.sql
@@ -1,2 +1,6 @@
-DELETE FROM employees
-WHERE emp_id IN (102,103,104,105);
+INSERT INTO employees
+(emp_id, emp_name, department, salary, joining_date)
+VALUES
+(102, 'sound', 'IT', 65000, '2024-06-10'),
+(103, 'hema', 'Finance', 70000, '2023-06-10'),
+(104, 'sharms', 'Sales', 55000, '2024-02-20');
