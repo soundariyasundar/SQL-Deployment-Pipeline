d645d85584fae7e769d8f8b87f478ae0b4ddca98
soundariya sundar <154726977+soundariyasundar@users.noreply.github.com>
2026-07-09 19:40:04 +0530

    Update salary insert statements in SQL query

--- cicd-sql/queries/update_salary.sql
+++ cicd-sql/queries/update_salary.sql
@@ -1,12 +1,11 @@
 INSERT INTO employees
-(
-    emp_id,
-    emp_name,
-    department,
-    salary,
-    joining_date
-)
+
+(emp_id, emp_name, department, salary, joining_date)
+
 VALUES
-(105, 'Mary', 'Finance', 70000, '2023-06-10'),
-(106, 'David', 'IT', 55000, '2024-02-20'),
-(107, 'Lisa', 'Sales', 65000, '2023-09-12');
+
+(108, 'John', 'IT', 65000, '2024-06-10'),
+
+(109, 'Mary', 'Finance', 70000, '2023-06-10'),
+
+(110, 'David', 'Sales', 55000, '2024-02-20');
