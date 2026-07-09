aca005665a877a74a801925eee247aa070049644
soundariya sundar <154726977+soundariyasundar@users.noreply.github.com>
2026-07-09 19:59:37 +0530

    Update employee records in update_salary.sql

--- cicd-sql/queries/update_salary.sql
+++ cicd-sql/queries/update_salary.sql
@@ -1,11 +1,6 @@
 INSERT INTO employees
-
 (emp_id, emp_name, department, salary, joining_date)
-
 VALUES
-
-(108, 'John', 'IT', 65000, '2024-06-10'),
-
-(109, 'Mary', 'Finance', 70000, '2023-06-10'),
-
-(110, 'David', 'Sales', 55000, '2024-02-20');
+(111, 'John', 'IT', 65000, '2024-06-10'),
+(112, 'Mary', 'Finance', 70000, '2023-06-10'),
+(113, 'David', 'Sales', 55000, '2024-02-20');
