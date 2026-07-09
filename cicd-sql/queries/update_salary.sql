f12be4768f39a9f16190bb0ceaac6999fe6825eb
soundariya sundar <154726977+soundariyasundar@users.noreply.github.com>
2026-07-09 20:11:48 +0530

    Change INSERT to SELECT for salary query
    Replaced INSERT statement with a SELECT query to retrieve employees with a salary greater than 60000.

--- cicd-sql/queries/update_salary.sql
+++ cicd-sql/queries/update_salary.sql
@@ -1,6 +1,3 @@
-INSERT INTO employees
-(emp_id, emp_name, department, salary, joining_date)
-VALUES
-(102, 'sound', 'IT', 65000, '2024-06-10'),
-(103, 'hema', 'Finance', 70000, '2023-06-10'),
-(104, 'sharms', 'Sales', 55000, '2024-02-20');
+SELECT *
+FROM employees
+WHERE Salary > 60000;
